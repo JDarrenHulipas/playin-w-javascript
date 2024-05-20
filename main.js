ccf4e855215c4d26d2861d36905dc3b926cd9aaf
Darren <darrenhulipas@gmail.com>
2024-05-20 22:11:06 +0200

    Adding some javascript code

---
+++ main.js
@@ -0,0 +1,10 @@
+let y = prompt("Insert a number");;
+let z = prompt("Insert a number");
+let x = z + y;
+console.log(x);
+
+if (x === 4) {
+  console.log("that's correct");
+} else {
+  console.log("that is not correct");
+}
