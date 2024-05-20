29437f2deb272ce3d7de7ab543513388617fa422
Darren <darrenhulipas@gmail.com>
2024-05-20 22:58:11 +0200

    adding selenium files

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-let y = prompt("Insert a number");;
-let z = prompt("Insert a number");
-let x = z + y;
-console.log(x);
+const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
 
-if (x === 4) {
-  console.log("that's correct");
-} else {
-  console.log("that is not correct");
-}
-//new comment 
+(async function example() {
+  let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+  const webdriver = require("selenium-webdriver");
+  /**
+   * Chrome tab
+   */
+  const chrome = require("selenium-webdriver/chrome");
+/**
+ * Firefox tab
+ */
+  const firefox = require("selenium-webdriver/firefox");
+  try {
+    await driver.get("https://www.wiris.com/en/");
+
+    //await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
+
+    //await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
