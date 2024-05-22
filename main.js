320f6a86922d7dbcaa64e9befe6450b26efc2808
Darren <darrenhulipas@gmail.com>
2024-05-23 00:24:43 +0200

    Closing cookies and pop up

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
+const { Builder, Browser, By, until } = require("selenium-webdriver");
 
 (async function example() {
   let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
@@ -6,17 +6,43 @@ const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
   /**
    * Chrome tab
    */
-  const chrome = require("selenium-webdriver/chrome");
-/**
- * Firefox tab
- */
+  //const chrome = require("selenium-webdriver/chrome");
+  /**
+   * Firefox tab
+   */
   const firefox = require("selenium-webdriver/firefox");
+  //
+
   try {
     await driver.get("https://www.wiris.com/en/");
+
+    let clickId = await driver.wait(
+      until.elementLocated(By.id("onetrust-pc-btn-handler")),
+      1000
+    );
+    await clickId.click();
+
+    // Find the element by class name and click it
+    let clickClassSave = await driver.findElement(
+      By.className("save-preference-btn-handler")
+    );
+    await clickClassSave.click();
+
+    let clickClassPum = await driver.findElement(
+      By.className("pum-close popmake-close")
+    );
+    await clickClassPum.click();
 
     //await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
 
     //await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
+
+    //js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+    let clickIda = await driver.wait(
+      until.elementLocated(By.id("onetrust-pc-btn-handler")),
+      1000
+    );
+    await clickIda.click();
   } finally {
     await driver.quit();
   }
