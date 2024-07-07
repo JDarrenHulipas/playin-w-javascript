96302741a1173742852b7de4647734060e1fa8a8
Darren <darrenhulipas@gmail.com>
2024-07-07 19:05:11 +0200

    Trying some code

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const { Builder, Browser, By, until } = require("selenium-webdriver");
 
+/**
+ * Selenium
+ */
 (async function example() {
   let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
   const webdriver = require("selenium-webdriver");
@@ -14,14 +17,25 @@ const { Builder, Browser, By, until } = require("selenium-webdriver");
   //
 
   try {
-    await driver.get("https://www.wiris.com/en/");
+    await driver.get("https://youtube.com/es");
+    noAccept = driver.findElement(By.linkText("Rechazar todo"));
+    await noAccept.click();
 
     let clickId = await driver.wait(
       until.elementLocated(By.id("onetrust-pc-btn-handler")),
-      1000
+      100000
     );
     await clickId.click();
+    /*
+    noAccept = driver.findElement(By.linkText("Rechazar todo"));
+noAccept.click();
+    
+    */
 
+    /**
+     * Fiding a button to click by link text */
+
+    /*
     // Find the element by class name and click it
     let clickClassSave = await driver.findElement(
       By.className("save-preference-btn-handler")
@@ -33,16 +47,20 @@ const { Builder, Browser, By, until } = require("selenium-webdriver");
     );
     await clickClassPum.click();
 
+
+
     //await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
 
     //await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
 
     //js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+
     let clickIda = await driver.wait(
       until.elementLocated(By.id("onetrust-pc-btn-handler")),
       1000
     );
     await clickIda.click();
+  */
   } finally {
     await driver.quit();
   }
